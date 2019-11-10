a202367884df4a01848c75c973cd4e9d22c96b20
Shixin Yang <shixiny@sfu.ca>
2019-11-09 23:14:59 -0800

    Setup jwt for user tokens

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    emial: {
+    email: {
         type: String,
         required: true,
         unique: true
