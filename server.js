7a056bef72cee1b76753b497fd7b3ab905cd0895
Shixin Yang <shixiny@sfu.ca>
2019-11-12 14:20:30 -0800

    Prepare for deploy

--- server.js
+++ server.js
@@ -1,24 +1,39 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 const app = express();
 
 
 //  Connect database
-connectDB(); 
+connectDB();
 
 //init  Middleware
-app.use(express.json({ extended: false}));
+app.use(express.json({
+    extended: false
+}));
 
 
 
 
-app.get('/',(req, res) => res.json({ msg: 'welcom to the app.'}))
+app.get('/', (req, res) => res.json({
+    msg: 'welcom to the app.'
+}))
 
 
 //define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+
+//serve  frontend 
+if (process.env.NODE_ENV === 'production') {
+    //set a static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+}
+
 
 const PORT = process.env.PORT || 5000;
 
