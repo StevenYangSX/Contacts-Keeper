4bdf9ec606b924f6bead62dfbedd4856a75049c5
Shixin Yang <shixiny@sfu.ca>
2019-11-11 19:32:30 -0800

    Created auth Context API and login/register form

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Link } from "react-router-dom"
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 const Navbar = ({ title, icon }) => {
   return (
     <div className="navbar bg-primary">
@@ -14,19 +14,25 @@ const Navbar = ({ title, icon }) => {
         <li>
           <Link to="/about">About</Link>
         </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
 Navbar.propTypes = {
   title: PropTypes.string.isRequired,
   icon: PropTypes.string
-}
+};
 
 Navbar.defaultProps = {
   title: "Contact Keeper",
   icon: "fas fa-id-card-alt"
-}
+};
 
-export default Navbar
+export default Navbar;
