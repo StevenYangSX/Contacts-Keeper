232f6cbc1e43484b6c53effeffd7657217ddb96d
Shixin Yang <shixiny@sfu.ca>
2019-11-12 14:24:31 -0800

    remove a useless route

--- server.js
+++ server.js
@@ -1,41 +1,33 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const path = require('path');
+const express = require("express");
+const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
-
 
 //  Connect database
 connectDB();
 
 //init  Middleware
-app.use(express.json({
+app.use(
+  express.json({
     extended: false
-}));
-
-
-
-
-app.get('/', (req, res) => res.json({
-    msg: 'welcom to the app.'
-}))
-
+  })
+);
 
 //define Routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
+//serve  frontend
+if (process.env.NODE_ENV === "production") {
+  //set a static folder
+  app.use(express.static("client/build"));
 
-//serve  frontend 
-if (process.env.NODE_ENV === 'production') {
-    //set a static folder
-    app.use(express.static('client/build'));
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
 }
 
-
 const PORT = process.env.PORT || 5000;
-
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
